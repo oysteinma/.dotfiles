4f9f6e75751a816343d63c09b4d7d2184d521849
Øystein Martinussen <69514063+oysteinma@users.noreply.github.com>
2023-04-30 17:19:11 +0200

    Formatting mac os script

--- scripts/mac_setup.zsh
+++ scripts/mac_setup.zsh
@@ -9,9 +9,10 @@ osascript -e 'tell application "System Preferences" to quit'
 # System Preferences > Dock
 
 echo "No delay when hiding/showing the Dock"
-defaults write com.apple.dock autohide-delay -float 0; defaults write com.apple.dock autohide-time-modifier -int 0
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -int 0
 
 killall Finder
 killall Dock
 
-echo "\n<<< macOS Setup Complete. A logout or restart might be necessary. >>>\n"
+echo "\n<<< macOS Setup Complete. Restart might be necessary. >>>\n"
