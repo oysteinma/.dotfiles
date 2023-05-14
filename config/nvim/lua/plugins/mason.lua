890b746af025d486966b027c6ce24119580d2091
Øystein Martinussen <69514063+oysteinma@users.noreply.github.com>
2023-05-14 16:12:50 +0200

    Border around nullls and fixed weird prettier bug

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,13 +1,8 @@
 return {
 	{
 		"williamboman/mason.nvim",
-		cmd = "Mason",
-		opts = function(_, opts)
-			table.insert(opts.ensure_installed, "prettierd")
-		end,
 		config = function()
 			require("mason").setup({
-				PATH = "prepend",
 				ui = {
 					border = "rounded",
 				},
