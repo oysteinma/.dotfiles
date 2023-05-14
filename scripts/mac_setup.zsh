e1bc87c3cc6d72bf16ffa857129a5794129c6829
Øystein Martinussen <69514063+oysteinma@users.noreply.github.com>
2023-05-14 23:52:25 +0200

    Really getting into macOS settings
    - Set up several settings, almost done with Finder settings

--- scripts/mac_setup.zsh
+++ scripts/mac_setup.zsh
@@ -1,16 +1,101 @@
 #!/usr/bin/env zsh
 
-# TODO: Set up proper settings
+# TODO: Continue setting up macOS, even though it is tedious
 
 echo "\n<<< Starting macOS Setup >>>\n"
 
+echo "\n<<< Administrator Password >>>\n"
+sudo -v
+
+echo "\n<<< Keep Alive >>>\n"
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+echo "\n<<< Running osascripts >>>\n"
+
 osascript -e 'tell application "System Preferences" to quit'
 
-# System Preferences > Dock
+echo "Setting wallpaper"
+# wallpaper="wallpaper.jpg"
+wallpaper="wallpaper2.png"
+osascript -e "tell application \"Finder\" to set desktop picture to \"${HOME}/.dotfiles/images/${wallpaper}\" as POSIX file"
+
+echo "\n<<< General Settings >>>\n"
+
+echo "Disable the sound effects on boot"
+sudo nvram SystemAudioVolume=" "
+
+echo "Bottom left screen corner → Show Desktop"
+defaults write com.apple.dock wvous-bl-corner -int 4
+defaults write com.apple.dock wvous-bl-modifier -int 0
+
+# FINDER
+
+echo "\n<<< Finder Settings >>>\n"
+
+echo "Be able to quit Finder"
+defaults write com.apple.finder QuitMenuItem -bool true
+
+echo "Disable the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+echo "Show all filename extensions"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo "Show status bar in Finder"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo "Show path bar in Finder"
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo "Display full POSIX path as Finder window title"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+echo "When performing a search, search the current folder by default"
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+echo "Disable the warning before emptying the Trash"
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+echo "Empty Trash securely by default"
+defaults write com.apple.finder FXRemoveOldTrashItems -bool true
+
+echo "Use list view in all Finder windows by default"
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+echo "Disable window animations and Get Info animations"
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+echo "Keep folders on top when sorting by name"
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+echo "Set user home as the default location for new Finder windows"
+defaults write com.apple.finder NewWindowTarget -string "PfLo"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
+
+echo "Don't show icons for hard drives, servers, and removable media on the desktop"
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
+
+echo "Open folders in tabs instead of new windows"
+defaults write com.apple.finder FinderSpawnTab -bool true
+
+echo "Don't show recent tags"
+defaults write com.apple.Finder ShowRecentTags -bool false
+
+# TODO: Deselect all tags, customize toolbar and sidebar
+
+# DOCK
+
+echo "\n<<< Dock Settings >>>\n"
 
 echo "No delay when hiding/showing the Dock"
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -int 0
+
+echo "Latency when dragging windows to the edge of the screen"
+defaults write com.apple.dock workspaces-edge-delay -float 60
 
 killall Finder
 killall Dock
