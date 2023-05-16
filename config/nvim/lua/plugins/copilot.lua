6f0b68936970c610a392c99100f6a9168f39945e
Øystein Martinussen <69514063+oysteinma@users.noreply.github.com>
2023-05-16 14:54:18 +0200

    Change copilot accept to shift + enter

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,20 +1,20 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		config = function()
-			require("copilot").setup({
-				suggestion = {
-					auto_trigger = true,
-					keymap = {
-						accept = "<TAB>",
-						accept_word = false,
-						accept_line = false,
-						dismiss = "<C-]>",
-					},
-				},
-			})
-		end,
-	},
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                suggestion = {
+                    auto_trigger = true,
+                    keymap = {
+                        accept = "<S-TAB>",
+                        accept_word = false,
+                        accept_line = false,
+                        dismiss = "<C-]>",
+                    },
+                },
+            })
+        end,
+    },
 }
