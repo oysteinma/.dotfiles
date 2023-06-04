695e3c0abe34d748417ff3989f1d33366ed9fe70
Øystein Martinussen <69514063+oysteinma@users.noreply.github.com>
2023-06-04 16:41:54 +0200

    Nvim changes
    - Tab from 4 to 2
    - Lsp no autoformate
    - Removed prettier

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.g.netrw_liststyle=3
-vim.g.netrw_banner=0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_banner = 0
 
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
@@ -10,9 +10,9 @@ vim.g.loaded_python3_provider = 0
 
 local opt = vim.opt
 
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
 opt.smartindent = true
 
